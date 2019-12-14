d78c210baab3e16e32dfcb9a8e66c93e8f7eb52f
Rougin Gutib <rougingutib@gmail.com>
2019-12-14 18:40:12 +0800

    Update Uri

--- src/Uri.php
+++ src/Uri.php
@@ -8,7 +8,7 @@ use Psr\Http\Message\UriInterface;
  * URI
  *
  * @package Zapheus
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class Uri implements UriInterface
 {
