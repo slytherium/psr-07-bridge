a58d049be0e31f95d5146b4de98eccfe768b1b30
Rougin Gutib <rougingutib@gmail.com>
2019-12-25 18:43:24 +0800

    Update StreamTest

--- tests/StreamTest.php
+++ tests/StreamTest.php
@@ -6,7 +6,7 @@ namespace Zapheus\Bridge\Psr;
  * Stream Test
  *
  * @package Slytherin
- * @author  Rougin Royce Gutib <rougingutib@gmail.com>
+ * @author  Rougin Gutib <rougingutib@gmail.com>
  */
 class StreamTest extends \PHPUnit_Framework_TestCase
 {
